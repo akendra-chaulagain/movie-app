28c8d5cb13c0d976029747ba1807e1d0d794322e
Akendra-Chaulagain <yakendrachaulagain31@gmail.com>
2022-04-17 09:38:52 +0545

    error page

--- client/src/pages/watch/Watch.js
+++ client/src/pages/watch/Watch.js
@@ -1,32 +1,21 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import Navbar from "../../components/navbar/Navbar";
 import "./Watch.css";
 import Play from "@material-ui/icons/PlayArrow";
 import Footer from "../../components/foooter/Footer";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllMovie } from "../../redux/apiCalls";
-import Search from "../../components/search/Search";
 
 // this page render to watch page when user click in watch now button in home page
 const Watch = () => {
   // get data from the given  url id
   const location = useLocation();
   const path = location.pathname.split("/")[2];
-  const dispatch = useDispatch();
-
-  // get all movies
-  useEffect(() => {
-    getAllMovie(dispatch);
-  }, [dispatch]);
-  const allmovie = useSelector((state) => state.movie.movies);
 
   const [watch, setWatch] = useState({});
   useEffect(() => {
     const getMove = async () => {
       try {
-        const res = await axios.get("/movies/find/" + path);
+        const res = await axios.get(`/api/movies/find/${path}`);
         setWatch(res.data);
       } catch (error) {
         console.log(error);
@@ -35,61 +24,44 @@ const Watch = () => {
     getMove();
   }, [path]);
 
-  // usestate for searchh
-  const [searchresult, setSearchresult] = useState([]);
-  const keys = ["title", "desc", "genre", "movieusername", "year", "duration"];
-  const searchData = (data) => {
-    return data.filter((item) =>
-      keys.some((key) => item[key].toLowerCase().includes(searchresult))
-    );
-  };
-
   return (
     <>
       <div className="watchContainer">
-        <Navbar setSearchresult={setSearchresult} />
-        {searchresult ? (
-          <Search data={searchData(allmovie)} />
-        ) : (
-          <>
-            <div className="container-fluid watch">
-              <div className="row">
-                <div className="col-md-5 left ">
-                  <div className="movieImg ">
-                    <img src={watch.img} alt="" />
+        <div className="container-fluid watch">
+          <div className="row">
+            <div className="col-md-5 left ">
+              <div className="movieImg ">
+                <img src={watch.img} alt="" />
 
-                    <div className="movietitle text-center">{watch.title}</div>
-                  </div>
-                </div>
-                <div className="col-md-6 right ">
-                  <h5>
-                    Name : <span>{watch.title}</span>
-                  </h5>
-                  <h5>
-                    genra : <span>{watch.genre}</span>
-                  </h5>
-                  <h5>
-                    Release data : <span>{watch.year}</span>
-                  </h5>
-                  <h5>
-                    Limit : <span className="limit-box">+{watch.limit}</span>
-                  </h5>
-                  <h5>
-                    Description : <span>{watch.desc}</span>
-                  </h5>
-                  <Link to={`/watch/movie/` + path}>
-                    <button>
-                      <Play />
-                      Watch
-                    </button>
-                  </Link>
-                </div>
+                <div className="movietitle text-center">{watch.title}</div>
               </div>
             </div>
-          </>
-        )}
+            <div className="col-md-6 right ">
+              <h5>
+                Name : <span>{watch.title}</span>
+              </h5>
+              <h5>
+                genra : <span>{watch.genre}</span>
+              </h5>
+              <h5>
+                Release data : <span>{watch.year}</span>
+              </h5>
+              <h5>
+                Limit : <span className="limit-box">+{watch.limit}</span>
+              </h5>
+              <h5>
+                Description : <span>{watch.desc}</span>
+              </h5>
+              <Link to={`/watch/movie/` + path}>
+                <button>
+                  <Play />
+                  Watch
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
-
       <Footer />
     </>
   );
